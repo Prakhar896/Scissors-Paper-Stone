382d9af637b7ddfdf60bb14f5e0b63c2f6d7367c
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-02-26 12:07:18 +0800

    Initial Commit

---
+++ Scissors Paper Stone/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Scissors Paper Stone
+//
+//  Created by Prakhar Trivedi on 26/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scissors Paper Stone/Scissors_Paper_StoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scissors_Paper_StoneApp.swift
+//  Scissors Paper Stone
+//
+//  Created by Prakhar Trivedi on 26/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Scissors_Paper_StoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
